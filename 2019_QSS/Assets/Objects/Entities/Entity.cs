8cb0cbeb1b2659ceb261281a862bd831c199c60a
pdj2428 <dj5219217@gmail.com>
2019-08-27 18:27:09 +0900

    Apply .gitignore

---
+++ 2019_QSS/Assets/Objects/Entities/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Entity :MonoBehaviour
+{
+    //protected Transform transform;
+    protected float hp;
+    protected float speed;
+    protected float DP;
+    protected float Power;
+
+    protected void Move()
+    {
+
+    }
+
+    protected void Attack()
+    {
+        
+    }
+
+    protected void Begin()
+    {
+        hp = 50;
+        
+    }
+}
