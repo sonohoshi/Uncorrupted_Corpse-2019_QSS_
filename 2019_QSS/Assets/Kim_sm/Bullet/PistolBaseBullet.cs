efcd0922b98e25f71933dab2c8f4e7d1bb6aa825
sonohoshi <yd111544@gmail.com>
2019-10-08 17:26:42 +0900

    Make Ammo Reload & Object pool
    Soooooooooooooo hard work.
    Ammo Reload!
    Object Pool!

--- 2019_QSS/Assets/Kim_sm/Bullet/PistolBaseBullet.cs
+++ 2019_QSS/Assets/Kim_sm/Bullet/PistolBaseBullet.cs
@@ -10,6 +10,8 @@ public static class PistolBaseBullet
     public static Transform BulletLocation;
     public static Transform GunRotation;
     private static Vector3 moveVector = new Vector3(0, 10, 0);
+    private static int dirBullet = 15;
+    private static bool reloadSwitch = false;
 
     private static readonly Vector3 addAngle = new Vector3(0, 0, -90);
 
@@ -37,8 +39,22 @@ public static class PistolBaseBullet
                 }
                 yield return new WaitForSeconds(0.1f);
             }
-
             yield return new WaitForEndOfFrame();
+        }
+    }
+    private static IEnumerator ReloadBullet()
+    {
+        if (dirBullet <= 0 && !reloadSwitch)
+        {
+            Debug.Log("Reloading");
+            reloadSwitch = true;
+            yield return new WaitForSeconds(5f);
+            reloadSwitch = false;
+            dirBullet = 15;
+        }
+        else if (dirBullet <= 0 && reloadSwitch) // 이미 재장전을 하고 있을 때는 재장전 중을 알려줌
+        {
+            Debug.Log("Already reloading");
         }
     }
 
@@ -49,11 +65,20 @@ public static class PistolBaseBullet
     
     public static void ShotBullet()
     {
-        BulletManager.BulletInfo bulletInfo = ObjectPoolManager.Dequeue(BulletManager.BulletType.Base);
-        bulletInfo.Bullet.position = BulletLocation.position;
-        bulletInfo.Bullet.eulerAngles = GunRotation.eulerAngles + addAngle;
-        bulletInfo.LiveTime = LiveTime;
-        bullets.Add(bulletInfo);
+        if (dirBullet > 0) // 현재 총알이 남아있을 경우
+        {
+            BulletManager.BulletInfo bulletInfo = ObjectPoolManager.Dequeue(BulletManager.BulletType.Base);
+            bulletInfo.Bullet.position = BulletLocation.position;
+            bulletInfo.Bullet.eulerAngles = GunRotation.eulerAngles + addAngle;
+            bulletInfo.LiveTime = LiveTime;
+            bullets.Add(bulletInfo);
+            dirBullet--;
+            Debug.Log(dirBullet);
+        }
+        else // 아니라면 재장전 메소드를 부른다
+        {
+            CoroutineManager.Instance.StartCoroutine(ReloadBullet());
+        }
     }
 
     public static void Initalize(Transform bulletLocation, Transform gunRotation)
@@ -61,4 +86,5 @@ public static class PistolBaseBullet
         BulletLocation = bulletLocation;
         GunRotation = gunRotation;
     }
+    
 }
