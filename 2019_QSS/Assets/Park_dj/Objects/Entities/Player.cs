75fb7b7a321a311eb3ba0fceacc431aea828578a
pdj2428 <49777470+pdj2428@users.noreply.github.com>
2019-11-08 16:52:24 +0900

    ChangePlayerScript
    .

--- 2019_QSS/Assets/Park_dj/Objects/Entities/Player.cs
+++ 2019_QSS/Assets/Park_dj/Objects/Entities/Player.cs
@@ -12,15 +12,7 @@ public class player : Entity
         BasePoint = 30;
         DistWeight = 1;
     }
-
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.W)) gameObject.transform.Translate(new Vector3(0, 3, 0) * Time.deltaTime);
-        if (Input.GetKey(KeyCode.S)) gameObject.transform.Translate(new Vector3(0, -3, 0) * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))gameObject.transform.Translate(new Vector3(3, 0, 0) * Time.deltaTime);
-        if(Input.GetKey(KeyCode.A))gameObject.transform.Translate(new Vector3(-3, 0, 0) * Time.deltaTime);
-    }
-
+    
     public float GetPriorityPoints(float distance)
     {
         return BasePoint - (distance * DistWeight);
@@ -36,20 +28,8 @@ public class player : Entity
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    public static void PlusHealth(int heal)
     {
-        if (collision.gameObject.CompareTag("FoodDepot"))
-        {
-            Food_Depot.interaction = true;
-        }      
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("fdDoor"))
-        {
-            NowFood_Health.problem = 0;
-            Food_Depot.interaction = false;
-        }
+        HP += heal;
     }
 }
