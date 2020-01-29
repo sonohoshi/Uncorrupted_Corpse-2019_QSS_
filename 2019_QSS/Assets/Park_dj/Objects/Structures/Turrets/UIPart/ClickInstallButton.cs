ef0f1f58c1fb1b5e80d059ccaef7c27b1c9a2454
pdj2428 <dj5219217@gmail.com>
2020-01-29 22:57:03 +0900

    MakeTurretUI
    .

---
+++ 2019_QSS/Assets/Park_dj/Objects/Structures/Turrets/UIPart/ClickInstallButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickInstallButton : MonoBehaviour
+{
+    public GameObject TurretUI;
+    public GameObject PutDescription;
+    public GameObject CancelButton;
+    public GameObject UIOnOffButton;
+    //첫번째 터렛
+    public void ClickFirstTurret()
+    {
+        InstallTurret selectTurret = GameObject.Find("AfterSelect").GetComponent<InstallTurret>();
+        PutDescription.SetActive(true);
+        CancelButton.SetActive(true);
+        selectTurret.Turret = 1;
+        UIOnOffButton.SetActive(false);
+        TurretUI.SetActive(false);
+    }
+    //두번째 터렛
+    public void ClickSecondTurret()
+    {
+        InstallTurret selectTurret = GameObject.Find("AfterSelect").GetComponent<InstallTurret>();
+        PutDescription.SetActive(true);
+        CancelButton.SetActive(true);
+        selectTurret.Turret = 2;
+        UIOnOffButton.SetActive(false);
+        TurretUI.SetActive(false);
+    }
+    //세번째 터렛
+    public void ClickThirdTurret()
+    {
+        InstallTurret selectTurret = GameObject.Find("AfterSelect").GetComponent<InstallTurret>();
+        PutDescription.SetActive(true);
+        CancelButton.SetActive(true);
+        selectTurret.Turret = 3;
+        UIOnOffButton.SetActive(false);
+        TurretUI.SetActive(false);
+    }
+}
---
+++ 2019_QSS/Assets/Park_dj/Objects/Structures/Turrets/UIPart/TurretUIButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretUIButton : MonoBehaviour
+{
+    public GameObject TurretUI;
+    public void OnOffTurretUI()
+    {
+        InstallTurret selectTurret = GameObject.Find("AfterSelect").GetComponent<InstallTurret>();
+        selectTurret.Turret= 0;//선택한 터렛 초기화
+        TurretUI.SetActive(!TurretUI.activeSelf);
+    }
+}
